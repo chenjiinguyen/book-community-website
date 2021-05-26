1c11ff52262f6a09fac4977bdb40f5dee8828873
chenjiinguyen <duyntp2000@gmail.com>
2021-05-26 13:52:52 +0700

    fix(router): remove controller in web router

--- app/routes/web.js
+++ app/routes/web.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const home = require("../app/controller/home");
+
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
