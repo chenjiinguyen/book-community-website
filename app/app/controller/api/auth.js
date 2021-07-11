e9bb5715a6b7902e5429057e9f826648c6a298f7
nhuquynh0711 <nhuquynhnt0711@gmail.com>
2021-07-11 22:32:33 +0700

    refactor: change data type response

--- app/app/controller/api/auth.js
+++ app/app/controller/api/auth.js
@@ -1,35 +1,36 @@
 const models = require("../../model/index");
 const type = require("../../../lib/type");
 const utils = require("../../../lib/utils");
-const moment = require('moment'); 
+const moment = require("moment");
 
 const controller = {
   signin: async (req, res, next) => {
-    const { username, password, remember} = req.query;
+    const { username, password, remember } = req.query;
     if (username && password) {
       let user = await models.user.findOne({ where: { username: username } });
       if (!user) {
-        res.json({ 
-            status: 200,
-            success: false,
-            message: "Người dùng không tồn tại"});
-      }else{
+        res.json({
+          status: 200,
+          success: false,
+          message: "Người dùng không tồn tại",
+        });
+      } else {
         if (await user.isValidPassword(password)) {
           // from now on we’ll identify the user by the id and the id is
-          // the only personalized value that goes into our tokend 
-          
-          const jwt = utils.issue_JWT(user,(remember == "true")?true:false);
-          res.json({ 
-              success: true,
-              user: user, 
-              token: jwt.token, 
-              expiresIn: jwt.expiresIn 
+          // the only personalized value that goes into our tokend
+
+          const jwt = utils.issue_JWT(user, remember == "true" ? true : false);
+          res.json({
+            success: true,
+            data: user,
+            token: jwt.token,
+            expiresIn: jwt.expiresIn,
           });
         } else {
-          res.json({ 
-              status: 200,
-              success: false,
-              message: "Mật khẩu không chính xác"
+          res.json({
+            status: 200,
+            success: false,
+            message: "Mật khẩu không chính xác",
           });
         }
       }
@@ -37,57 +38,64 @@ const controller = {
       res.json({
         status: 200,
         success: false,
-        message: "Vui lòng nhập tất cả các trường"
-       });
+        message: "Vui lòng nhập tất cả các trường",
+      });
     }
   },
   signup: async (req, res, next) => {
-    const attributes = ["username","email","password","name","gender","birthday"];
+    const attributes = [
+      "username",
+      "email",
+      "password",
+      "name",
+      "gender",
+      "birthday",
+    ];
     const param = req.query;
     let result = {
-      status : 200,
-      success : false,
-      message : ""
+      status: 200,
+      success: false,
+      message: "",
     };
-    if(utils.check_properties_in_object(attributes,param)){
+    if (utils.check_properties_in_object(attributes, param)) {
       let data = await models.user.findOne({
         where: {
-          username : param.username,
-        }
-      })
-      if(data == null){
+          username: param.username,
+        },
+      });
+      if (data == null) {
         let data_email = await models.user.findOne({
           where: {
-            email : param.email,
-          }
+            email: param.email,
+          },
         });
-        if(data_email == null){
+        if (data_email == null) {
           let user = await models.user.create({
-            username : param.username,
-            email : param.email,
+            username: param.username,
+            email: param.email,
             gender: param.gender,
-            password : param.password,
-            name : param.name,
-            birthday : moment(param.birthday,["DD-MM-YYYY", "DD/MM/YYYY"]),
-            avatar : "https://ui-avatars.com/api/?size=256&name="+param.name,
+            password: param.password,
+            name: param.name,
+            birthday: moment(param.birthday, ["DD-MM-YYYY", "DD/MM/YYYY"]),
+            avatar: "https://ui-avatars.com/api/?size=256&name=" + param.name,
           });
-          if(user){
+          if (user) {
             result.success = true;
-            result.user = user;
-          }else{
+            result.data = user;
+          } else {
             result.message = "Có lỗi trong quá trình đăng ký. Vui lòng thử lại";
           }
-        }else{
+        } else {
           result.message = "Email đã tồn tại";
         }
-      }else{
+      } else {
         result.message = "Tên người dùng đã tồn tại";
       }
-    }else{
-      result.message =  "Vui lòng nhập tất cả các trường";
+    } else {
+      result.message = "Vui lòng nhập tất cả các trường";
     }
     res.json(result);
-  }
+  },
 };
 
 module.exports = controller;
